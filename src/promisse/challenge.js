efcc65584fbb8661efa1961af979c67abae68617
José Manuel Ferrández Cascales <mrjark@MacBook-de-MrJark.local>
2022-10-20 07:33:14 +0200

    copia de json

--- src/promisse/challenge.js
+++ src/promisse/challenge.js
@@ -1,7 +1,6 @@
 //hemos instalado en la terminal el fetch para así poder llamarlo desde aquí, lo hemos intalado mediante la terminal como; npm i node-fetch
 
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 
 const API = 'https://api.escuelajs.co/api/v1';
 
-HashChangeEvent
